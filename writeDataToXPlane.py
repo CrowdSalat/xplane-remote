77214eb86c3cf188aa8734ffb8a13ccf54d0b6b9
crowdsalat <janweyrich93@gmail.com>
2019-09-28 15:08:11 +0200

    added RREF

--- writeDataToXPlane.py
+++ writeDataToXPlane.py
@@ -31,7 +31,9 @@ import array as arr
 
 ip = "192.168.23.192"
 port = 49000
-header = b"DREF\0"
+HEADER_DREF = b"DREF\0"
+HEADER_RREF = b"RREF\0"
+
 
 
 def set_position_x():
@@ -40,16 +42,15 @@ def set_position_x():
      '''
      value_override = 1
      dref_override = add_filler_bytes(b"sim/operation/override/override_planepath\0")
-     msgLocalX = header + struct.pack('f', value_override) + dref_override
+     msgLocalX = HEADER_DREF + struct.pack('f', value_override) + dref_override
      send_message(msgLocalX)
 
      value_x_offset = -100
      stringLocalX = add_filler_bytes(b"sim/flightmodel/position/local_x\0")
-     msgLocalX = header + struct.pack('f', value_x_offset)+ stringLocalX
+     msgLocalX = HEADER_DREF + struct.pack('f', value_x_offset)+ stringLocalX
      send_message(msgLocalX)
 
-def add_filler_bytes(dref_name: bytes) -> bytes:
-     xplane_dref_expected_byte_lenght = 500
+def add_filler_bytes(dref_name: bytes, xplane_dref_expected_byte_lenght = 500) -> bytes:
      return dref_name + ((xplane_dref_expected_byte_lenght - len(dref_name)) * b' ')
 
 def send_message(byte_message):
@@ -65,7 +66,7 @@ def set_altitude(altitude=1000.0):
      altitude_dref = add_filler_bytes(b'sim/flightmodel/position/local_z\0')
 
      altitude_value = altitude
-     msg =  header + struct.pack('f', altitude_value) + altitude_dref
+     msg =  HEADER_DREF + struct.pack('f', altitude_value) + altitude_dref
      send_message(msg)
 
 def set_longi_lati_coordinates(x=50.941357, y=6.958307):
@@ -73,17 +74,56 @@ def set_longi_lati_coordinates(x=50.941357, y=6.958307):
      Passiert nichts. Alerdings sah das Cockpit nach dem Stezen nicht ganz korrekt aus.
      '''
      x_dref = add_filler_bytes(b'sim/flightmodel/position/longitude\0')
-     msg =  header + struct.pack('f', x) + x_dref
+     msg =  HEADER_DREF + struct.pack('f', x) + x_dref
      send_message(msg)
 
      y_dref = add_filler_bytes(b'sim/flightmodel/position/latitude\0')
-     msg =  header + struct.pack('f', y) + y_dref
+     msg =  HEADER_DREF + struct.pack('f', y) + y_dref
      send_message(msg)
 
 
+def read_datarefs():
+'''
+klappt nicht. Laut GUI klappt das empfangen am server socket, 
+aber der hier geoeffnete client sockte akzeptiert keine Verbindung. Laut Serverlogs scheint der port auch trtz des Binds nicht fix zu sein.
+
+möglichkeiten:
+1. iptables mal alle UDP port incoming oeffnen
+2. Einfach die GUI zum schreiben verwenden und hier vorerst nur schreiben, damit man mal voran kommt.
+'''
+     #activate
+     dref_freq = struct.pack('i', 1)
+     dref_en = struct.pack('i', 1)
+     x_dref = add_filler_bytes(b'sim/flightmodel/position/longitude\0', 400)
+
+     msg = HEADER_RREF + dref_freq  + dref_en + x_dref
+
+     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     sock.bind(('', 49000))
+     sock.connect((ip, port))
+     input("Press Enter to continue...")
+     sock.sendall(msg)
+     print('send')
+     #read
+     print('receive')
+     data, addr = sock.recv(1024)
+     print('received from ' + addr)
+     print('data ' + data)
+
+     # deactivate
+     dref_freq = struct.pack('i', 0) 
+     msg = HEADER_RREF + dref_freq  + dref_en + x_dref
+     sock.send(msg)
+     sock.close() 
+
+
+
+
 if __name__ == "__main__":
-     set_position_x()
+     #set_position_x()
      
+
      #set_altitude(2001)
 
      #set_longi_lati_coordinates()
+     read_datarefs()
