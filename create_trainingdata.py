5891f53e7dbd53cc3db9a43a0a28b7ecbdec7465
Jan Weyrich <jan.weyrich@protonmail.com>
2019-10-17 15:34:35 +0100

    new datasets

--- create_trainingdata.py
+++ create_trainingdata.py
@@ -61,14 +61,14 @@ def trainings_set02():
     altitudes = [1000, 1500, 2000, 2500, 3000, 3500,  4000, 4500,  5000]
     heading_changes = [90.0, -90.0]
     bank_modes = [1, 2, 3, 4]
-    maneuvers = xp.define_flight_maneuvers(altitudes, [0], [500.0], heading_changes, bank_modes , sort=True)
+    maneuvers = xp.define_flight_maneuvers(altitudes, [0], [600.0], heading_changes, bank_modes , sort=True)
     xp.fly(maneuvers)
 
 def trainings_set03():
     '''
     fly parable in 100 difference and then climb to 500ft 
     '''
-    logger.info('run trainings_set02')
+    logger.info('run trainings_set03')
     altitudes = [1500, 2500,  3500,  4500]
     climb = [100, -100]
     climb_rates = [600, 500, 400, 300, 200]
@@ -81,7 +81,7 @@ def trainings_set04():
     '''
     fly banks while climbing and falling
     '''
-    logger.info('run trainings_set02')
+    logger.info('run trainings_set04')
     altitudes = [1000, 2000, 3000, 4000, 5000]
     climb = [-100]
     climb_rates = [600, 500, 400]
@@ -92,6 +92,52 @@ def trainings_set04():
 
     xp.fly(maneuvers)
 
+
+def trainings_set05():
+    # parable with trotthle=100%
+    logger.info('run trainings_set05')
+    xp.set_throttle(1.0)
+
+    altitudes = [1250, 1750, 2250, 2750, 3250, 3750, 4250, 4750]
+    climb = [-100]
+    climb_rates = [300, 400, 500, 600]
+
+    maneuvers = xp.define_flight_maneuvers(altitudes, climb, climb_rates, [0], [0], sort=True)
+    xp.fly(maneuvers)
+
+
+def trainings_set06():
+    '''
+    parable flights between 900ft and 1500ft
+    '''
+    logger.info('run trainings_set06')
+
+    xp.set_throttle(1.0)
+
+    altitudes = [-1]
+    heading_changes = [0]
+    bank_modes = [0]
+    climb = [-100, 200 , -100 , 200, -100, 200 , -100 , 200, -100, 200 , -100 , 200, -600]
+    climb_rates = [300, 400, 500, 600]
+    maneuvers = xp.define_flight_maneuvers(altitudes, climb, climb_rates, heading_changes, bank_modes , sort=True)
+    print(maneuvers)
+    xp.fly(maneuvers)
+
+def trainings_set07():
+    '''
+    bank flight on constant height: 90 degree both directions; 5,10,15,20 bank angle
+    spiral up to new altitude with 500fpm
+    '''
+    logger.info('run trainings_set07')
+
+    xp.set_throttle(1.0)
+
+    altitudes = [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500,  5000]
+    heading_changes = [25.0, -25.0]
+    bank_modes = [1, 2, 3, 4]
+    maneuvers = xp.define_flight_maneuvers(altitudes, [0], [500.0], heading_changes, bank_modes , sort=True)
+    xp.fly(maneuvers)
+
 def main():
     config_logger()
     xp.init_xp_remote()
@@ -100,7 +146,9 @@ def main():
     #trainings_set01()
     #trainings_set02()
     #trainings_set03()
-    trainings_set04()
+    #trainings_set04()
+    #trainings_set05()
+    trainings_set06()
 
     xp.close_xp_remote()
      
